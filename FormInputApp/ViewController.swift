9224ea52050d3e49035477a2e543a6e97435acc2
Gurjot Saini <gurjotsaini1992@gmail.com>
2024-01-29 19:21:34 -0500

    Initial Commit

---
+++ FormInputApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FormInputApp
+//
+//  Created by Gurjot Saini on 29/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FormInputAppUITests/FormInputAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FormInputAppUITestsLaunchTests.swift
+//  FormInputAppUITests
+//
+//  Created by Gurjot Saini on 29/01/24.
+//
+
+import XCTest
+
+final class FormInputAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
