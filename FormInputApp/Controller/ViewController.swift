481e7cc68cf39744bbe83944ec1f131361861c34
Gurjot Saini <gurjotsaini1992@gmail.com>
2024-01-30 19:32:08 -0500

    first commit

---
+++ FormInputApp/Controller/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  FormInputApp
+//
+//  Created by Gurjot Saini on 29/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    
+    @IBOutlet weak var userInfoTextView: UITextView!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func AddButtonAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func submitButtonAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func clearButtonAction(_ sender: Any) {
+        
+    }
+}
+
---
+++ FormInputApp/Model/DataHandleModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataHandleModel.swift
+//  FormInputApp
+//
+//  Created by Gurjot Saini on 29/01/24.
+//
+
+import Foundation
